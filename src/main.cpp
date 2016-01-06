9a40367dbe6acedc142d48289fa5f10465386704
moschulze <mo.schulze@gmx.net>
2016-01-06 07:39:24 +0100

    configured logging format

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include "easylogging++.h"
-#include "configuration/Configuration.h"
-#include "configuration/ConfigurationLoader.h"
 
 INITIALIZE_EASYLOGGINGPP
 
+void configureLogging();
+
 int main(int argv, char* argc[]) {
     START_EASYLOGGINGPP(argv, argc);
+    configureLogging();
 
     return 0;
+}
+
+void configureLogging() {
+    el::Configurations defaultConf;
+    defaultConf.setToDefault();
+    defaultConf.set(
+            el::Level::Global,
+            el::ConfigurationType::Format,
+            "%datetime{%d.%M.%Y %H:%m:%s} [%level]: %msg"
+    );
+    el::Loggers::reconfigureLogger("default", defaultConf);
 }
